1b86e232865fad38dfa7f40081890e29bbff2dba
Xujing <547358880@qq.com>
2022-03-07 11:19:05 +0800

    first commit

---
+++ src/main/java/com/ericashy/jenkinstest/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ericashy.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/ericashy/jenkinstest/BasicTest.java
@@ -0,0 +1,11 @@
+package com.ericashy.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+
+    @Test
+    public void test1() {
+        System.out.println("dasdasd");
+    }
+}
