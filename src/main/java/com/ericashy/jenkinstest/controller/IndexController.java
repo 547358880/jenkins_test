0a6c1a8b950225a422af6a6aafa79e8def7d6bc8
Xujing <547358880@qq.com>
2022-03-07 11:24:27 +0800

    add index page

---
+++ src/main/java/com/ericashy/jenkinstest/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.ericashy.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "This is index page!";
+    }
+
+}
